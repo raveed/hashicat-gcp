3a060bb002c05b09a0638d306adf9b0380541540
dinesh <krdinesh26@gmail.com>
2021-02-24 15:47:34 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "network" {
+  source  = "app.terraform.io/dinesh-training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+  project_id   = var.project
+  network_name = "example-vpc"
+  routing_mode = "GLOBAL"
+  subnets = [
+    {
+      subnet_name   = "dinesh-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+}
